921413ba86175ef952e4a716f194fb898b9f0512
Manuel Odendahl <wesen@ruinwesen.com>
2024-01-18 16:16:17 +0100

    :sparkles: Add ls commands and easy way to hook up repository commands to cobra

--- pkg/cmds/ls-commands/ls-commands.go
+++ pkg/cmds/ls-commands/ls-commands.go
@@ -1,3 +1,144 @@
 package ls_commands
 
-type ListCommandsCommand struct{}
+import (
+	"context"
+	glazed_cmds "github.com/go-go-golems/glazed/pkg/cmds"
+	"github.com/go-go-golems/glazed/pkg/cmds/alias"
+	"github.com/go-go-golems/glazed/pkg/cmds/layers"
+	"github.com/go-go-golems/glazed/pkg/middlewares"
+	"github.com/go-go-golems/glazed/pkg/middlewares/row"
+	"github.com/go-go-golems/glazed/pkg/settings"
+	"github.com/go-go-golems/glazed/pkg/types"
+	"github.com/pkg/errors"
+)
+
+type AddCommandToRowFunc func(cmd glazed_cmds.Command, row types.Row, parsedLayers *layers.ParsedLayers) ([]types.Row, error)
+
+type ListCommandsCommand struct {
+	*glazed_cmds.CommandDescription
+	commands            []glazed_cmds.Command
+	AddCommandToRowFunc AddCommandToRowFunc
+}
+
+var _ glazed_cmds.GlazeCommand = (*ListCommandsCommand)(nil)
+
+type ListCommandsCommandOption func(*ListCommandsCommand) error
+
+func WithCommandDescriptionOptions(options ...glazed_cmds.CommandDescriptionOption) ListCommandsCommandOption {
+	return func(q *ListCommandsCommand) error {
+		description := q.CommandDescription.Description()
+		for _, option := range options {
+			option(description)
+		}
+		return nil
+	}
+}
+
+func WithAddCommandToRowFunc(f AddCommandToRowFunc) ListCommandsCommandOption {
+	return func(q *ListCommandsCommand) error {
+		q.AddCommandToRowFunc = f
+		return nil
+	}
+}
+
+func NewListCommandsCommand(
+	allCommands []glazed_cmds.Command,
+	options ...ListCommandsCommandOption,
+) (*ListCommandsCommand, error) {
+	glazeParameterLayer, err := settings.NewGlazedParameterLayers(
+		settings.WithFieldsFiltersParameterLayerOptions(
+			layers.WithDefaults(
+				&settings.FieldsFilterFlagsDefaults{
+					Fields: []string{"name", "type", "short", "source"},
+				},
+			),
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	options_ := append([]glazed_cmds.CommandDescriptionOption{
+		glazed_cmds.WithLayersList(glazeParameterLayer),
+	})
+
+	ret := &ListCommandsCommand{
+		commands: allCommands,
+		CommandDescription: glazed_cmds.NewCommandDescription(
+			"ls-commands",
+			options_...,
+		),
+	}
+
+	for _, option := range options {
+		err := option(ret)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return ret, nil
+}
+
+func (q *ListCommandsCommand) RunIntoGlazeProcessor(
+	ctx context.Context,
+	parsedLayers *layers.ParsedLayers,
+	gp middlewares.Processor,
+) error {
+	tableProcessor, ok := gp.(*middlewares.TableProcessor)
+	if !ok {
+		return errors.New("expected a table processor")
+	}
+
+	// check if there already is a ReorderColumnOrderMiddleware in the row processors
+	hasReorderColumnOrderMiddleware := false
+	for _, middleware := range tableProcessor.RowMiddlewares {
+		if _, ok := middleware.(*row.ReorderColumnOrderMiddleware); ok {
+			hasReorderColumnOrderMiddleware = true
+			break
+		}
+	}
+	if !hasReorderColumnOrderMiddleware {
+		tableProcessor.AddRowMiddleware(
+			row.NewReorderColumnOrderMiddleware(
+				[]string{"name", "short", "long", "source", "query"}),
+		)
+	}
+
+	for _, command := range q.commands {
+		description := command.Description()
+		obj := types.NewRow(
+			types.MRP("name", description.Name),
+			types.MRP("short", description.Short),
+			types.MRP("long", description.Long),
+			types.MRP("source", description.Source),
+			types.MRP("type", "unknown"),
+			types.MRP("parents", description.Parents),
+		)
+
+		switch c := command.(type) {
+		case *alias.CommandAlias:
+			obj.Set("aliasFor", c.AliasFor)
+			obj.Set("type", "alias")
+		default:
+		}
+
+		rows := []types.Row{obj}
+		if q.AddCommandToRowFunc != nil {
+			var err error
+			rows, err = q.AddCommandToRowFunc(command, obj, parsedLayers)
+			if err != nil {
+				return err
+			}
+		}
+
+		for _, row_ := range rows {
+			err := gp.AddRow(ctx, row_)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
--- pkg/repositories/helpers.go
+++ pkg/repositories/helpers.go
@@ -1,10 +1,13 @@
 package repositories
 
 import (
+	"fmt"
+	"github.com/go-go-golems/glazed/pkg/cli"
 	"github.com/go-go-golems/glazed/pkg/cmds"
 	"github.com/go-go-golems/glazed/pkg/cmds/alias"
 	"github.com/go-go-golems/glazed/pkg/cmds/loaders"
 	"github.com/go-go-golems/glazed/pkg/help"
+	"github.com/spf13/cobra"
 	"os"
 )
 
@@ -54,4 +57,48 @@ func LoadCommandsFromInputs(
 	}
 
 	return commands, nil
+}
+
+func LoadRepositories(
+	helpSystem *help.HelpSystem,
+	rootCmd *cobra.Command,
+	repositories_ []*Repository,
+	options ...cli.CobraParserOption,
+) []cmds.Command {
+
+	allCommands := []cmds.Command{}
+
+	for _, repository := range repositories_ {
+		err := repository.LoadCommands(helpSystem)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Error initializing commands: %s\n", err)
+			os.Exit(1)
+		}
+
+		aliases := []*alias.CommandAlias{}
+		commands := []cmds.Command{}
+		commands_ := repository.CollectCommands([]string{}, true)
+
+		for _, command := range commands_ {
+			switch v := command.(type) {
+			case *alias.CommandAlias:
+				aliases = append(aliases, v)
+			case cmds.Command:
+				commands = append(commands, v)
+			}
+		}
+
+		allCommands = append(allCommands, commands_...)
+
+		err = cli.AddCommandsToRootCommand(
+			rootCmd, commands, aliases,
+			options...,
+		)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Error initializing commands: %s\n", err)
+			os.Exit(1)
+		}
+
+	}
+	return allCommands
 }
